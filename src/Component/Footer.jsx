9ae551fc1f2effc5c4316b926599cafa7f425e4e
fojletuhin <fojletuhin@gmail.com>
2024-04-26 12:34:02 +0600

    add tostify, helmet, reactIcon

---
+++ src/Component/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BsFacebook, BsGithub, BsInstagram } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <div className="bg-[#1F2744] px-4 md:px-8 lg:px-[100px] py-16 grid md:grid-cols-3 gap-6 justify-center text-white">
+            <div>
+                <Link to='/'><a className="btn btn-ghost text-xl font-bold">Rangdhanu Art</a></Link>
+                <p className="mt-9 text-[#9D9EA1]">Customer Supports:</p>
+                <p>(+880) 1877127477</p>
+                <p className="mt-3 text-[#9D9EA1]">Taltola, Agargaon <br /> Dhaka 1207</p>
+                <p className="mt-3">fojletuhin@gmail.com</p>
+                <div className="flex gap-12 mt-5 mb-5 text-4xl">
+                    <BsFacebook />
+                    <BsInstagram />
+                    <BsGithub />
+                </div>
+                <p>© 2024 . All rights reserved.</p>
+            </div>
+            <div>
+                <p className="font-semibold mt-2 mb-8 text-xl text-center md:text-left">Properties</p>
+                <ul>
+                    <li className="mb-2">About us</li>
+                    <li className="mb-2">Our Mission</li>
+                    <li className="mb-2">Privacy Policy</li>
+                    <li className="mb-2">Help Center</li>
+                    <li className="mb-2">Contact us</li>
+                </ul>
+            </div>
+            <div>
+                <p className="font-semibold mt-2 mb-8 text-xl text-center md:text-left">All pages</p>
+                <ul>
+                    <li className="mb-2">Landscape Painting</li>
+                    <li className="mb-2">Portrait Drawing</li>
+                    <li className="mb-2">Watercolour Painting</li>
+                    <li className="mb-2">Oil Painting</li>
+                    <li className="mb-2">Charcoal Sketching</li>
+                    <li className="mb-2">Cartoon Drawing</li>
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Home from './Pages/Home';
+import Root from './Pages/Root';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+
+const router= createBrowserRouter([
+  {
+    path:'/',
+    element:<Root></Root>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/login',
+        element:<Login></Login>
+      },
+      {
+        path:'/register',
+        element:<Register></Register>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<RouterProvider router={router}></RouterProvider>  
+</React.StrictMode>,
 )
