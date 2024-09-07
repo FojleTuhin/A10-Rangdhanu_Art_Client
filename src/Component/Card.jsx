0cb909cfeacd0fecdebf586eb33bb025ac33a94b
fojletuhin <fojletuhin@gmail.com>
2024-09-08 01:38:46 +0600

    add hover on card

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -19,16 +19,16 @@ const Card = ({ item }) => {
         <div>
             <div>
 
-                <div className="card w-[300px]  bg-white bg-opacity-80 shadow-xl ">
+                <div className="card group w-[300px]  bg-white bg-opacity-80 shadow-xl hover:bg-[#16325B] hover:text-white">
                     <figure className="px-6 pt-6">
                         <img src={image} className="rounded-xl h-[252px] w-[252px]" />
                     </figure>
-                    <div className="card-body ">
+                    <div className="card-body">
                         <h2 className="card-title text-[16px]">{subcategory}</h2>
                         <div className="flex items-center justify-between">
 
                             <p className="text-xl font-bold">{price} tk</p>
-                            <p className="text-xl font-bold text-right text-[#16325B]">Stock: {stockStatus}</p>
+                            <p className="text-xl font-bold text-right ">Stock: {stockStatus}</p>
 
                         </div>
 
@@ -40,10 +40,34 @@ const Card = ({ item }) => {
                         <hr className=" mt-1 mb-1" />
 
                         <div className="">
-                            <Link to={`viewDetails/${_id}`}><button className="btn bg-[#16325B] border-none text-white w-full">View details</button></Link>
+                            <Link to={`viewDetails/${_id}`}><button className="btn bg-[#16325B] group-hover:bg-white group-hover:text-[#16325B] border-none text-white w-full">View details</button></Link>
                         </div>
                     </div>
                 </div>
+
+                {/* <div className="card group w-[300px] bg-white bg-opacity-80 shadow-xl hover:bg-[#16325B] hover:text-white">
+                    <figure className="px-6 pt-6">
+                        <img src={image} className="rounded-xl h-[252px] w-[252px]" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-[16px]">{subcategory}</h2>
+                        <div className="flex items-center justify-between">
+                            <p className="text-xl font-bold">{price} tk</p>
+                            <p className="text-xl font-bold text-right">Stock: {stockStatus}</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <FaStar />
+                            <p>{rating}</p>
+                        </div>
+                        <hr className="mt-1 mb-1" />
+                        <div className="">
+                            <Link to={`viewDetails/${_id}`}>
+                                <button className="btn bg-[#16325B] border-none text-white w-full group-hover:bg-white group-hover:text-[#16325B]">View details</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div> */}
+
             </div>
         </div>
     );
