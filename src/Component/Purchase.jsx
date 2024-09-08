2a9878d7c35dffc61169823a6bc0e3fc1cbd44c1
fojletuhin <fojletuhin@gmail.com>
2024-09-08 21:25:52 +0600

    update purchase ui

--- src/Component/Purchase.jsx
+++ src/Component/Purchase.jsx
@@ -6,21 +6,21 @@ const Purchase = () => {
       
       
     return (
-        <div className="px-4 md:px-8 lg:px-[100px] pt-10 pb-10  mt-14 md:grid grid-cols-2 lg:grid-cols-3  gap-10">
+        <div className="px-4 md:px-8 lg:px-[100px]  my-14 flex flex-wrap justify-center gap-10">
             
-            <div className="bg-[#F8F6F1] px-10 py-10 rounded-2xl mt-5 md:mt-0">
+            <div className="bg-[#16325B] text-white p-5 w-[280px] rounded-2xl mt-5 md:mt-0">
                 <img src="https://i.ibb.co/LZVytGz/Frame-1000001713.png" alt="" />
                 <p className="mt-7 mb-7 font-bold text-xl">Robust ID Verification.</p>
                 <p>ID verified breeders and buyers to make our site even safer and more transparent.</p>
 
             </div>
-            <div className="bg-[#F8F6F1] px-10 py-10 rounded-2xl mt-5 md:mt-0">
+            <div className="bg-[#16325B] text-white p-5 w-[280px] rounded-2xl mt-5 md:mt-0">
                 <img src="https://i.ibb.co/2cpM4yc/Frame-1000001713-1.png" alt="" />
                 <p className="mt-7 mb-7 font-bold text-xl">The safest way to pay.</p>
                 <p>Purchase with peace of mind with our Art Guarantee.</p>
 
             </div>
-            <div className="bg-[#F8F6F1] px-10 py-10 rounded-2xl mt-5 md:mt-0">
+            <div className="bg-[#16325B] text-white p-5 w-[280px] rounded-2xl mt-5 md:mt-0">
                 <img src="https://i.ibb.co/cDkJXZ0/Frame-1000001713-2.png" alt="" />
                 <p className="mt-7 mb-7 font-bold text-xl">Trust and safety team.</p>
                 <p>A dedicated team to ensure Art safety.</p>
