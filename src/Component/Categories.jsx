35c107e94af0e87ce555ed4193ade67376895151
fojletuhin <fojletuhin@gmail.com>
2024-09-08 21:15:25 +0600

    update popular craft card ui

--- src/Component/Categories.jsx
+++ src/Component/Categories.jsx
@@ -42,16 +42,16 @@ const Categories = () => {
                         <div key={categorie._id}>
 
                             <Link to={`/similarCategories/${categorie._id}`}>
-                                <div className="card w-[300px]  shadow-xl ">
-                                    <figure className="px-6 pt-6">
-                                        <img src={categorie.image} className="rounded-xl" />
+                                <div className="card group w-[300px] h-[435px]  bg-[#16325B] text-white hover:bg-opacity-80 hover:bg-white hover:text-black shadow-xl ">
+                                    <figure className="px-6 pt-6 ">
+                                        <img src={categorie.image} className="rounded-xl h-[252px] w-[252px]" />
                                     </figure>
                                     <div className="card-body ">
                                         <h2 className="card-title">{categorie.itemName}</h2>
                                         <div className="flex items-center justify-between">
 
                                             <p className="text-xl font-bold">{categorie.price} tk</p>
-                                            <p className="text-xl font-bold text-right text-[#4F95FF]">Stock: {categorie.stockStatus}</p>
+                                            <p className="text-xl font-bold text-right text-white group-hover:text-black">Stock: {categorie.stockStatus}</p>
 
                                         </div>
 
@@ -60,7 +60,7 @@ const Categories = () => {
                                             <p>{categorie.rating}</p>
                                         </div>
 
-                                        <hr className="bg-[#D1D1D1] mt-3 mb-3" />
+                                        
 
                                        
                                     </div>
--- src/Component/Popular.jsx
+++ src/Component/Popular.jsx
@@ -18,19 +18,19 @@ const Popular = () => {
                 />
             </p>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-8 gap-4">
-                <div data-aos="fade-right" className="bg-[#D9D9D9] rounded-xl ">
+                <div  className="bg-[#D9D9D9] rounded-xl ">
                     <img className="h-[270px] w-[250px] rounded-t-xl" src="https://i.ibb.co/kqD6f9K/353916793-249970294426807-4451598891261957386-n.jpg" alt="" />
                     <p className="mt-4 mb-4 text-center">Most famous</p>
                 </div>
-                <div data-aos="fade-right" className="bg-[#D9D9D9] rounded-xl ">
+                <div  className="bg-[#D9D9D9] rounded-xl ">
                     <img className="h-[270px] w-[250px] rounded-t-xl" src="https://i.ibb.co/yk1wz3b/436986258-798510442162064-7164722731068788277-n.jpg" alt="" />
                     <p className="mt-4 mb-4 text-center">Most famous</p>
                 </div>
-                <div data-aos="fade-left" className="bg-[#D9D9D9] rounded-xl">
+                <div  className="bg-[#D9D9D9] rounded-xl">
                     <img className="h-[270px] w-[250px] rounded-t-xl" src="https://i.ibb.co/hsfnKX8/385524991-324124630386814-490333696148519164-n.jpg" alt="" />
                     <p className="mt-4 mb-4 text-center">Most famous</p>
                 </div>
-                <div data-aos="fade-left" className="bg-[#D9D9D9] rounded-xl justify-center">
+                <div  className="bg-[#D9D9D9] rounded-xl justify-center">
                     <img className="h-[270px] w-[250px] rounded-t-xl" src="https://i.ibb.co/TbCxf7r/361543682-634210018673836-4282878231442221567-n.jpg" alt="" />
                     <p className="mt-4 mb-4 text-center">Most famous</p>
                 </div>
