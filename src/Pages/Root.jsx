8dbcb541047f4425bf8122d93d30269dbf520f94
fojletuhin <fojletuhin@gmail.com>
2024-09-10 02:04:14 +0600

    add scrolltotop component

--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Component/Footer";
 import Navbar from "../Component/Navbar";
+import ScrollToTop from "../Component/ScrollToTop";
 
 const Root = () => {
     return (
         <div className="bg-[url('https://i.ibb.co.com/jgn8z3w/flat-mountains.png')]  bg-cover bg-center bg-no-repeat bg-fixed ">
+            <ScrollToTop></ScrollToTop>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
