622e8b0e127b054d3b619bdb2c986bcfa0b759d7
fojletuhin <fojletuhin@gmail.com>
2024-09-10 20:11:27 +0600

    add padding

--- src/Pages/SimilarCategories.jsx
+++ src/Pages/SimilarCategories.jsx
@@ -24,7 +24,7 @@ const SimilarCategories = () => {
 
 
     return (
-        <div className='bg-[#F8F6F1] px-4 md:px-8 lg:px-[100px] pb-10 text-black '>
+        <div className='bg-[#F8F6F1] px-4 md:px-8 lg:px-[100px] pt-16 pb-10 text-black '>
             <div className="bg-[#EBFBE5] text-[#3EA570] py-4">
                 <h1 className="font-bold text-xl text-center">Similar Categories</h1>
             </div>
